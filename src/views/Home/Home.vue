49302b264a6e00167c62976ebdf68882342da258
ayiooo <coderxialuo@gmail.com>
2020-12-24 00:20:53 +0800

    使用vue-awesome-swiper完成轮播图开发

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="home">
-    <VHeader >
+    <VHeader>
       <template #center>
         <span>首页</span>
       </template>
     </VHeader>
+    <HomeSwiper :swiperList="swiperList"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import VHeader from 'cm/VHeader'
+import HomeSwiper from './childComps/HomeSwiper'
 import { getHomeData } from 'network/home'
 export default {
   name: 'Home',
   components: {
-    VHeader
+    VHeader,
+    HomeSwiper
+  },
+  data () {
+    return {
+      swiperList: []
+    }
   },
   mounted () {
     this.getData()
@@ -24,12 +32,12 @@ export default {
     async getData () {
       const data = await getHomeData()
       console.log(data)
+      this.swiperList = data.swiperList
+      console.log(this.swiperList)
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.box {
-}
 </style>
---
+++ src/views/Home/childComps/HomeSwiper.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="home__swiper">
+    <swiper ref="mySwiper" :options="swiperOptions">
+      <swiper-slide v-for="item in swiperList" :key="item.id"
+        ><img :src="item.imgUrl" alt=""
+      /></swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+
+export default {
+  name: 'HomeSwiper',
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  props: {
+    swiperList: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home__swiper {
+  height: 0;
+  padding-bottom: 31.5%;
+  font-size: 0;
+  overflow: hidden;
+  img {
+    width: 100%;
+  }
+}
+
+</style>
