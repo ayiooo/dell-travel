93a43cd41dd8a80ad07c0a1943cf64b070c86891
ayiooo <coderxialuo@gmail.com>
2020-12-23 23:15:14 +0800

    完成头部组件的开发

---
+++ src/components/common/VHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+    <div class="header__left">
+      <slot name="left"></slot>
+    </div>
+    <div class="header__center">
+      <slot name="center"></slot>
+    </div>
+    <div class="header__right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/scss/variables.scss';
+.header {
+  display: flex;
+  align-items: center;
+  height: 0.43rem;
+  background-color: $color-primary;
+  font-size: 0.2rem;
+  text-align: center;
+  line-height: 0.43rem;
+  color: #fff;
+  &__left,
+  &__right {
+    min-width: 0.5rem;
+  }
+  &__center {
+    flex: 1;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="home">
-    home
+    <VHeader >
+      <template #center>
+        <span>首页</span>
+      </template>
+    </VHeader>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import VHeader from 'cm/VHeader'
 export default {
   name: 'Home',
   components: {
+    VHeader
   },
   mounted () {
 
